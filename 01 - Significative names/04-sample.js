a116c3b11336eb51dfc39b948ea670fdd00e2e40
Gabriel Felix Paim <gabrielfelixpaim@gmail.com>
2024-10-16 20:19:56 -0300

    Update 04-sample.js

--- 01 - Significative names/04-sample.js
+++ 01 - Significative names/04-sample.js
@@ -1,6 +1,6 @@
-let fcr = function(a, b) {
-    return a + b;
+let retornoDaFuncaoSoma = function(numb1, numb2) {
+    return numb1 + numb2;
 }
 
-let rsl = fcr(3, 5);
-console.log(rsl);
+let resultado = retornoDaFuncaoSoma(3, 5);
+console.log(resultado);
